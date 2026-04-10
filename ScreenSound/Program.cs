5ffe4e1f3cdce537e119fe6732ca0c6a59162092
Lucas Oliveira Souza <lucasosouza41@gmail.com>
2026-04-10 14:36:38 -0300

    primeiro commit

---
+++ ScreenSound/Program.cs
@@ -0,0 +1,13 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Manchild";
+musica1.artista = "Sabrina Carpinteira";
+
+Console.WriteLine($"O nome da música é {musica1.nome}");
+Console.WriteLine($"O nome do artista é {musica1.artista}");
+
+Musica musica2 = new Musica();
+musica2.nome = "You rock my word";
+musica2.artista = "Michael Jackson";
+
+Console.WriteLine($"\nO nome da música é {musica2.nome}");
+Console.WriteLine($"O nome do artista é {musica2.artista}");
