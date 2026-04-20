94d50f5b6cc69e94a10d848e506b5c587453db83
Lucas <83218223+Lucas-2110@users.noreply.github.com>
2026-04-20 16:51:57 -0300

    Último commit do projeto

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,15 +1,38 @@
-﻿Album albumDoQueen = new Album();
-albumDoQueen.Nome = "A night at the opera";
+﻿/*Banda queen = new Banda("Queen");
 
-Musica musica1 = new Musica();
-musica1.Nome = "Bohemian Rhapsody";
-musica1.Duracao = 354;
+Album albumDoQueen = new Album("A night at the opera");
 
-Musica musica2 = new Musica();
-musica2.Nome = "Love of my life";
-musica2.Duracao = 213;
+Musica musica1 = new Musica(queen, "Bohemian Rhapsody")
+{
+    Duracao = 354,
+    Disponivel = true,
+};
+
+Musica musica2 = new Musica(queen, "Love of my life")
+{
+    Duracao = 213,
+    Disponivel = false,
+};
 
 albumDoQueen.AdicionarMusica(musica1);
 albumDoQueen.AdicionarMusica(musica2);
+queen.AdicionarAlbum(albumDoQueen);
 
 albumDoQueen.ExibirMusicasDoAlbum();
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+queen.ExibirDiscografia();
+*/
+
+Episodio ep1 = new(1, "Técnica de facilitação", 45);
+ep1.AdicionarConvidado("Gustavo Guanabara");
+ep1.AdicionarConvidado("Ana Clara");
+
+Episodio ep2 = new(1, "Técnica de aprendizado", 67);
+ep1.AdicionarConvidado("Fernando");
+ep1.AdicionarConvidado("Lucas");
+
+Podcast podcast = new("Podcast Especial", "Lucas");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
